8f6bf50f8cfd95ff44552e59666883d0376f95f5
Seby Tomita <94014609+Sebytomita@users.noreply.github.com>
2025-03-14 11:04:09 +0200

    Create ProjectType.java

---
+++ Tema2/ProjectType.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tema2;
+
+/**
+ * @author Seby
+ */
+
+public enum ProjectType {
+    THEORETICAL, PRACTICAL
+}
