d65cba3d0e2fb615b0971d4e4b78c905befe5855
Seby Tomita <94014609+Sebytomita@users.noreply.github.com>
2025-03-14 11:02:38 +0200

    Create Person.java

---
+++ Tema2/Person.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tema2;
+
+import java.time.LocalDate;
+
+/**
+ * @author Seby
+ */
+abstract class Person {
+    private String name;
+    private LocalDate dateOfBirth;
+
+    public Person(String name, LocalDate dateOfBirth) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getName() { return name; }
+    public LocalDate getDateOfBirth() { return dateOfBirth; }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Person)) return false;
+        Person other = (Person) obj;
+        return name.equals(other.name) && dateOfBirth.equals(other.dateOfBirth);
+    }
+
+   
+}
