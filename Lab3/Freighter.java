310684fe011deb6a9328bda3080c95f8da0bd554
Seby Tomita <94014609+Sebytomita@users.noreply.github.com>
2025-03-14 11:18:52 +0200

    Create Freighter.java

---
+++ Lab3/Freighter.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab3;
+
+/**
+ *
+ * @author Seby
+ */
+public class Freighter extends Aircraft implements CargoCapable {
+    private double maxPayload;
+
+    public Freighter(String model, String callSign, double maxPayload) {
+        super(model, callSign);
+        this.maxPayload = maxPayload;
+    }
+
+    @Override
+    public double getMaxPayload() {
+        return maxPayload;
+    }
+
+    @Override
+    public String toString() {
+        return "Freighter [model=" + getModel() + ", callSign=" + getCallSign() + 
+               ", maxPayload=" + maxPayload + "]";
+    }
+}
