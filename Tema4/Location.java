77ed0db3ba5ce74137ad80901788f8a6d2439750
Seby Tomita <94014609+Sebytomita@users.noreply.github.com>
2025-03-28 11:06:29 +0200

    Create Location.java

---
+++ Tema4/Location.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab4;
+
+/**
+ *
+ * @author Seby
+ */
+public class Location implements Comparable<Location> {
+    private String name;
+    private LocationType type;
+
+    public Location(String name, LocationType type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocationType getType() {
+        return type;
+    }
+
+    public boolean isFriendly() {
+        return type == LocationType.FRIENDLY;
+    }
+
+    @Override
+    public int compareTo(Location other) {
+        return this.name.compareTo(other.name);
+    }
+
+    @Override
+    public String toString() {
+        return "Location{name='" + name + "', type=" + type + "}";
+    }
+}
